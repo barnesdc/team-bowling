2b8f21d69c53c590447512aa963ee55c47d66d8f
dantebarnes <dcbarnes@aggies.ncat.edu>
2018-09-19 15:49:48 -0400

    first commit with new u/i

---
+++ README.md
@@ -0,0 +1,9 @@
+#Task
+Create a project plan:
+-weekly tasking for each team mate
+-Task #, Id, who to complete it, and end date
+
+Requirments/Specification Document:
+-due next Tuesday
+
+Advisory Meeting Weekly
--- www/js/app.js
+++ www/js/app.js
@@ -3,24 +3,99 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular
+  .module("app", [
+    "ionic",
+    "app.controllers",
+    "app.routes",
+    "app.directives",
+    "app.services"
+  ])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs).
-    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
-    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
-    // useful especially with forms, though we would prefer giving the user a little more room
-    // to interact with the app.
-    if (window.cordova && window.Keyboard) {
-      window.Keyboard.hideKeyboardAccessoryBar(true);
-    }
+  .config(function($ionicConfigProvider, $sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist([
+      "self",
+      "*://www.youtube.com/**",
+      "*://player.vimeo.com/video/**"
+    ]);
+  })
 
-    if (window.StatusBar) {
-      // Set the statusbar to use the default style, tweak this to
-      // remove the status bar on iOS or change it to use white instead of dark colors.
-      StatusBar.styleDefault();
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (
+        window.cordova &&
+        window.cordova.plugins &&
+        window.cordova.plugins.Keyboard
+      ) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
+  })
+
+  /*
+  This directive is used to disable the "drag to open" functionality of the Side-Menu
+  when you are dragging a Slider component.
+*/
+  .directive("disableSideMenuDrag", [
+    "$ionicSideMenuDelegate",
+    "$rootScope",
+    function($ionicSideMenuDelegate, $rootScope) {
+      return {
+        restrict: "A",
+        controller: [
+          "$scope",
+          "$element",
+          "$attrs",
+          function($scope, $element, $attrs) {
+            function stopDrag() {
+              $ionicSideMenuDelegate.canDragContent(false);
+            }
+
+            function allowDrag() {
+              $ionicSideMenuDelegate.canDragContent(true);
+            }
+
+            $rootScope.$on("$ionicSlides.slideChangeEnd", allowDrag);
+            $element.on("touchstart", stopDrag);
+            $element.on("touchend", allowDrag);
+            $element.on("mousedown", stopDrag);
+            $element.on("mouseup", allowDrag);
+          }
+        ]
+      };
     }
+  ])
+
+  /*
+  This directive is used to open regular and dynamic href links inside of inappbrowser.
+*/
+  .directive("hrefInappbrowser", function() {
+    return {
+      restrict: "A",
+      replace: false,
+      transclude: false,
+      link: function(scope, element, attrs) {
+        var href = attrs["hrefInappbrowser"];
+
+        attrs.$observe("hrefInappbrowser", function(val) {
+          href = val;
+        });
+
+        element.bind("click", function(event) {
+          window.open(href, "_system", "location=yes");
+
+          event.preventDefault();
+          event.stopPropagation();
+        });
+      }
+    };
   });
-})
