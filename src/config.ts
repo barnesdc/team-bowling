7ef74b849615b270f2bd91eab02ae3eb29b19160
dantebarnes <dcbarnes@aggies.ncat.edu>
2018-10-22 01:31:33 -0500

    .

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+// firebase API key information
+export const firebaseConfig = {
+  fire: {
+    apiKey: "AIzaSyBkjK4MXok4pe7HemheEPsQJ2x_miQia6Q",
+    authDomain: "bowling-app-b45c0.firebaseapp.com",
+    databaseURL: "https://bowling-app-b45c0.firebaseio.com",
+    projectId: "bowling-app-b45c0",
+    storageBucket: "bowling-app-b45c0.appspot.com",
+    messagingSenderId: "49362757433"
+  }
+};
