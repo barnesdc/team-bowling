090774da7953a0a4032bdcd5fd1def155a6e7252
mdtrotter <mtrotter@aggies.ncat.edu>
2018-11-19 23:10:45 -0500

    Fixed teams number display and added Score label to teams page

--- src/pages/teams/teams.ts
+++ src/pages/teams/teams.ts
@@ -68,7 +68,7 @@ export class TeamsPage {
         for (var i = 0; i < teams.length; i++) {
           if (this.checked.indexOf(teams[i]["bowler_id"]) > -1) {
             showTeams[count] = teams[i];
-            showTeams[count]["team_id"] = teamNum;
+            showTeams[count]["team_id"] = null;
             showTeams[count]["score"] = 0;
             count++;
             console.log(showTeams[count - 1]["bowler_name"]);
@@ -122,7 +122,9 @@ export class TeamsPage {
             }
 
             console.log("Creating Team "+teamNum);
-            this.database.CreateTeams(teamNum, showTeams[count-3], showTeams[count-2], showTeams[count-1]);
+            console.log("Team Members "+showTeams[i-2][""])
+            this.database.CreateTeams(teamNum, showTeams[i-2], showTeams[i-1], showTeams[i]);
+            showTeams[i-2]["team_id"] = showTeams[i-1]["team_id"] = showTeams[i]["team_id"] = teamNum;
             teamNum++;
             handicapCount = 0;
           }
