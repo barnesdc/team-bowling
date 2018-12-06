0a29b53349b20fa7b5555ddf49bb3db7c2740c66
mdtrotter <mtrotter@aggies.ncat.edu>
2018-12-06 01:40:57 -0500

    Scoring Fixed

--- src/pages/games-scores/games-scores.ts
+++ src/pages/games-scores/games-scores.ts
@@ -12,20 +12,10 @@ export class GamesScoresPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   Teams: any;
-  public winningTeam = 0;
-  public winningMembers = ["", "", ""];
-  public secondTeam = 0;
-  public secondMembers = ["", "", ""];
-  public thirdTeam = 0;
-  public thirdMembers = ["", "", ""];
-  public tieTeams = ["", ""];
-  public winningScore = 0;
-  public secondScore = 0;
-  public thirdScore = 0;
+  public rank = [];
+  public totalScores = [];
   ShowWinner = false;
-  ShowSecond = false;
-  ShowThird = false;
-  tie = false;
+  public count: any;
 
   //retrieve data sent from teams page
   ionViewDidLoad() {
@@ -42,110 +32,35 @@ export class GamesScoresPage {
 
   //function gathers individual scores and determines first, second and third placed teams
   calculateWinner() {
-    console.log("Calculating Winners");
-    console.log(this.Teams);
-    var maxScore = -1,
-      score = 0,
-      secondScore = 0,
-      thirdScore = 0;
-
-    //adds individual scores for each team and calculates total scores
-    for (var i = 0; i < this.Teams.length; i += 3) {
-      score =
-        parseInt(this.Teams[i].score) +
-        parseInt(this.Teams[i + 1].score) +
-        parseInt(this.Teams[i + 2].score);
-      console.log("Score: " + score + " for Team " + this.Teams[i].team_id);
-
-      //function to determine if a tie between first and second has occurred
-      if (score == maxScore) {
-        this.tie = true;
-        this.tieTeams[1] = this.Teams[i].team_id;
-      }
-
-      //function if a new first place team is found. Moves second place team to third and first place team to second and pushes new team score and members into first.
-      else if (score > maxScore) {
-        maxScore = score;
-        this.winningScore = score;
-        this.tie = false;
-        this.thirdTeam = this.secondTeam;
-        this.secondTeam = this.winningTeam;
-        this.winningTeam = this.Teams[i].team_id;
-
-        this.thirdMembers[0] = this.secondMembers[0];
-        this.thirdMembers[1] = this.secondMembers[1];
-        this.thirdMembers[2] = this.secondMembers[2];
-
-        this.secondMembers[0] = this.winningMembers[0];
-        this.secondMembers[1] = this.winningMembers[1];
-        this.secondMembers[2] = this.winningMembers[2];
-
-        this.winningMembers[0] = this.Teams[i].bowler_name;
-        this.winningMembers[1] = this.Teams[i + 1].bowler_name;
-        this.winningMembers[2] = this.Teams[i + 2].bowler_name;
-        this.tieTeams[0] = this.Teams[i].team_id;
-      }
-
-      //function if a new second place team is found
-      else if (score > secondScore) {
-        console.log("Second Team");
-        thirdScore = secondScore;
-        secondScore = score;
-        this.thirdScore = this.secondScore;
-        this.secondScore = score;
-        this.thirdTeam = this.secondTeam;
-        this.secondTeam = this.Teams[i].team_id;
-
-        this.thirdMembers[0] = this.secondMembers[0];
-        this.thirdMembers[1] = this.secondMembers[1];
-        this.thirdMembers[2] = this.secondMembers[2];
-
-        this.secondMembers[0] = this.Teams[i].bowler_name;
-        this.secondMembers[1] = this.Teams[i + 1].bowler_name;
-        this.secondMembers[2] = this.Teams[i + 2].bowler_name;
-
-        this.ShowSecond = true;
-        console.log(this.secondTeam);
-      }
-
-      //function if a new third place team is found
-      else if (score > thirdScore) {
-        console.log("Third Team");
-        thirdScore = score;
-        this.thirdScore = score;
-
-        this.thirdTeam = this.Teams[i].team_id;
-
-        this.thirdMembers[0] = this.Teams[i].bowler_name;
-        this.thirdMembers[1] = this.Teams[i + 1].bowler_name;
-        this.thirdMembers[2] = this.Teams[i + 2].bowler_name;
-        this.ShowThird = true;
-        console.log(this.thirdTeam);
+    console.log("Calculating Winners "+this.Teams.length);
+    this.ShowWinner = true;
+    var score = 0;
+    
+    for(var i=0; i<this.Teams.length; i+=3){
+      console.log("Pass score");
+      score = parseInt(this.Teams[i].score) + parseInt(this.Teams[i+1].score) + parseInt(this.Teams[i+2].score);
+      if(this.rank.length == 0){
+        this.rank.push(i);
+        this.totalScores.push(score);
+      }else{
+        for(var j=0; j<this.rank.length; j++){
+          console.log("loop");
+          if(score > this.totalScores[j]){
+            console.log("2");
+            this.rank.splice(j, 0, i);
+            this.totalScores.splice(j, 0, score);
+            break;
+        }
+          if(j == this.rank.length-1){
+            console.log("3");
+            this.rank.splice(this.rank.length, 0, i);
+            this.totalScores.splice(this.totalScores.length, 0, score);
+            break;
+          }
+        }
       }
     }
-    console.log(
-      "Winning Team is Team " +
-        this.winningTeam +
-        ": " +
-        this.winningMembers[0] +
-        " " +
-        this.winningMembers[1] +
-        " " +
-        this.winningMembers[2]
-    );
-    console.log(
-      "Second Place is Team" +
-        this.secondTeam +
-        ": " +
-        this.secondMembers[0] +
-        " " +
-        this.secondMembers[1] +
-        " " +
-        this.secondMembers[2]
-    );
-    if (!this.tie) {
-      //ShowWinner prevents html being shown unless calculateWinners function is called
-      this.ShowWinner = true;
-    }
+    console.log("ranks "+this.rank);
+    console.log("scores "+this.totalScores);
   }
 }
